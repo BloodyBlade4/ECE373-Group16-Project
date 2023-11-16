650d8c1d29644927e055d8325b08e0072e668e84
Cole <98437396+BloodyBlade4@users.noreply.github.com>
2023-11-15 21:32:52 -0700

    Update WindowCreateAccount.java

--- project/src/main/java/gui/WindowCreateAccount.java
+++ project/src/main/java/gui/WindowCreateAccount.java
@@ -1,20 +1,26 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+import javax.swing.JCheckBox;
+import java.awt.FlowLayout;
 
 public class WindowCreateAccount extends JFrame{
 	
@@ -24,6 +30,7 @@ public class WindowCreateAccount extends JFrame{
 	private static final long serialVersionUID = 1L;
 	private JTextField textFieldUsername;
 	private JPasswordField textFieldPassword;
+	private JCheckBox showPasswordCheckBox;
 	
 	
 	public WindowCreateAccount(ActionListener submitCreateAccount, 
@@ -34,6 +41,7 @@ public class WindowCreateAccount extends JFrame{
 		this.setSize(300, 400);
 		this.setLocationRelativeTo(null); //Center of screen.
 		getContentPane().setLayout(new BorderLayout(0, 0));
+		this.setResizable(false);
 		/* End initialization */
 		
 		JPanel panelFooter = new JPanel();
@@ -42,6 +50,26 @@ public class WindowCreateAccount extends JFrame{
 		JPanel panelCenter = new JPanel();
 		getContentPane().add(panelCenter, BorderLayout.CENTER);
 		panelCenter.setLayout(new BoxLayout(panelCenter, BoxLayout.Y_AXIS));
+		
+		JLabel label1 = new JLabel("");
+		label1.setHorizontalAlignment(SwingConstants.CENTER);
+		label1.setBounds(628, 28, 169, 125);
+		try {
+			ImageIcon Icon = new ImageIcon(new ImageIcon(WindowMenu.class.getResource("/main/java/gui/Lock Icon.png")).getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)); //100, 100 add your own size
+			label1.setIcon(Icon);
+		
+		} catch (Exception e) {
+			System.out.println("Unable to load image.");
+		}
+		JSeparator separator_2 = new JSeparator();
+		panelCenter.add(separator_2);
+		
+		JLabel lblNewLabel = new JLabel("LockBox");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(new Color(46, 139, 87));
+		lblNewLabel.setFont(new Font("Avenir Next", Font.PLAIN, 16));
+		panelCenter.add(lblNewLabel);
+		panelCenter.add(label1);
 		
 		JSeparator separator = new JSeparator();
 		panelCenter.add(separator);
@@ -55,10 +83,10 @@ public class WindowCreateAccount extends JFrame{
 		panelField.setMaximumSize(new Dimension(200, 30));
 		panelFields.add(panelField);
 		
-		JLabel lblNewLabel_1 = new JLabel("Username");
-		panelField.add(lblNewLabel_1);
-		
 		textFieldUsername = new JTextField();
+		textFieldUsername.setForeground(Color.GRAY);
+		textFieldUsername.setFont(new Font("Avenir Next", Font.PLAIN, 13));
+		textFieldUsername.setText("Username");
 		panelField.add(textFieldUsername);
 		textFieldUsername.setColumns(10);
 		//end panel field
@@ -66,24 +94,29 @@ public class WindowCreateAccount extends JFrame{
 		JPanel panelField_1 = new JPanel();
 		panelField_1.setMaximumSize(new Dimension(200, 30));
 		panelFields.add(panelField_1);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("Password");
-		panelField_1.add(lblNewLabel_1_1);
+		panelField_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		textFieldPassword = new JPasswordField();
+		textFieldPassword.setForeground(Color.GRAY);
+		textFieldPassword.setFont(new Font("Avenir Next", Font.PLAIN, 13));
+		textFieldPassword.setText("Password");
 		textFieldPassword.setColumns(10);
 		panelField_1.add(textFieldPassword);
 		
-		JCheckBox showPasswordCheckBox = new JCheckBox("Show Password");
+		showPasswordCheckBox = new JCheckBox("");
         showPasswordCheckBox.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // Toggle the echo character of the password field
                 textFieldPassword.setEchoChar(showPasswordCheckBox.isSelected() ? 0 : '*');
             }
         });
+        showPasswordCheckBox.setSize(10, 10);
+        panelField_1.add(showPasswordCheckBox);
 		
 		/*BUTTONS */
 		JButton btnSubmit = new JButton("Create Account");
+		btnSubmit.setForeground(new Color(46, 139, 87));
+		btnSubmit.setFont(new Font("Avenir Next", Font.PLAIN, 13));
 		btnSubmit.addActionListener(submitCreateAccount);
 		btnSubmit.setAlignmentX(Component.CENTER_ALIGNMENT);
 		panelCenter.add(btnSubmit);
@@ -95,12 +128,12 @@ public class WindowCreateAccount extends JFrame{
 		JButton btnForgotPassword = Styling.navigationButton("Forgot Password");
 		btnForgotPassword.addActionListener(forgotPasswordStateChange);
 		panelFooter.add(btnForgotPassword);
-		
 		/* END buttons */
 		
 		
 		JSeparator separator_1 = new JSeparator();
 		panelCenter.add(separator_1);
+		
 		
 	}
 
